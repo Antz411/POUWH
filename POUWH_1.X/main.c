cd6cded3b1c7e8cf5e9e58bcf9aadeddc6307a81
DESKTOP-S5RF0KJ\ANTZ <antonysm.tse@gmail.com>
2018-02-15 13:22:20 +0200

    First GPIO test firmware - RA2 not switching

--- POUWH_1.X/main.c
+++ POUWH_1.X/main.c
@@ -4,13 +4,14 @@
  * Created: 14-02-2018, 12:17 PM
  */
 // CONFIG1
-#pragma config FOSC = ECH       //  (ECH, External Clock, High Power Mode (4-32 MHz); device clock supplied to CLKIN pin)
+#pragma config FOSC = INTOSC    //  (INTOSC oscillator; I/O function on CLKIN pin)
 #pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-#pragma config MCLRE = ON       // MCLR Pin Function Select (MCLR/VPP pin function is MCLR)
+#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
 #pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
 #pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset disabled)
 #pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+
 // CONFIG2
 #pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
 #pragma config PLLEN = OFF      // PLL Enable (4x PLL disabled)
@@ -20,8 +21,69 @@
 #pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
 
 #include <xc.h>
+#include "main.h"
 
-void main(void) {
-    // Made some changes
+/**********************************
+            GLOBALS
+**********************************/
+void initializers(void);
+void Delay_Sec(unsigned char sec);
+void blink_LED(void);
+
+/**********************************
+        INITIALISE MCU
+**********************************/
+void initializers(void)
+{
+    /*** set corresponding tris bits to inputs and outputs ***/
+	TRISA = 0x03;                   // (MCLR/ICSP = Inputs) 0000 0011
+	PORTA = 0X00;                   // Clear all Ports
+    /* OSC SET UP */
+    //OSCCON = 0x60;                // IRCF Bits for 4MHz (pg 42)
+}
+
+/* Delay for X seconds */
+void Delay_Sec(unsigned char sec)
+{
+    unsigned char x, y;
+    
+    for(y = 0; y < sec; y++){
+        for(x = 0; x < 100; x++){
+            __delay_ms(10);
+        }
+    }
+}
+
+void blink_LED(void)
+{
+    STATUS_LED_ON;
+    LED0_ON;
+    LED1_ON;
+    LED2_ON;
+    LED4_ON;
+    TEMP_OUT_ON;
+    Delay_Sec(1);
+    
+    STATUS_LED_OFF;
+    LED0_OFF;
+    LED1_OFF;
+    LED2_OFF;
+    LED4_OFF;
+    TEMP_OUT_OFF;
+    Delay_Sec(1);
+}
+
+/**********************************
+            MIAN FINCTION
+**********************************/
+void main(void) 
+{
+    initializers();
+    
+    blink_LED();
+    
+    while(1) {
+        blink_LED();
+    }
     return;
 }
--- POUWH_1.X/main.h
+++ POUWH_1.X/main.h
@@ -3,54 +3,44 @@
  * Author:      A.Smith
  * Comments:    
  */
-
-// This is a guard condition so that contents of this file are not included
-// more than once.  
-#ifndef XC_HEADER_TEMPLATE_H
-#define	XC_HEADER_TEMPLATE_H
-
-#include <xc.h> // include processor files - each processor file is guarded.  
-
-// TODO Insert appropriate #include <>
-
-// TODO Insert C++ class definitions if appropriate
-
-// TODO Insert declarations
-
-// Comment a function and leverage automatic documentation with slash star star
-/**
-    <p><b>Function prototype:</b></p>
-  
-    <p><b>Summary:</b></p>
-
-    <p><b>Description:</b></p>
-
-    <p><b>Precondition:</b></p>
-
-    <p><b>Parameters:</b></p>
-
-    <p><b>Returns:</b></p>
-
-    <p><b>Example:</b></p>
-    <code>
- 
-    </code>
-
-    <p><b>Remarks:</b></p>
- */
-// TODO Insert declarations or function prototypes (right here) to leverage 
-// live documentation
+ #ifndef MAIN_H
+#define	MAIN_H
 
 #ifdef	__cplusplus
 extern "C" {
-#endif /* __cplusplus */
+#endif
+    
+    #ifndef _XTAL_FREQ
+    #define _XTAL_FREQ    4000000     // 8Mhz INTOSC internal osc
+    #define __delay_us(x) _delay((unsigned long)((x)*(_XTAL_FREQ/4000000.0)))
+    #endif
+    
+    #define LED0                     RA0                  // OUTPUT
+    #define LED0_ON                  LED0             = 1
+    #define LED0_OFF                 LED0             = 0
+    
+    #define LED1                     RA1                  // OUTPUT
+    #define LED1_ON                  LED1             = 1
+    #define LED1_OFF                 LED1             = 0
+    
+    #define LED2                     RA2                  // OUTPUT
+    #define LED2_ON                  LED2             = 1
+    #define LED2_OFF                 LED2             = 0
 
-    // TODO If C++ is being used, regular C code needs function names to have C 
-    // linkage so the functions can be used by the c code. 
+    #define LED4                     RA4                  // OUTPUT
+    #define LED4_ON                  LED4             = 1
+    #define LED4_OFF                 LED4             = 0
 
+    #define STATUS_LED              RA3                  // OUTPUT
+    #define STATUS_LED_ON           STATUS_LED      = 1
+    #define STATUS_LED_OFF          STATUS_LED      = 0
+    
+    #define TEMP_OUT                RA5                  // OUTPUT
+    #define TEMP_OUT_ON             TEMP_OUT        = 1
+    #define TEMP_OUT_OFF            TEMP_OUT        = 0
+    
 #ifdef	__cplusplus
 }
 #endif /* __cplusplus */
 
 #endif	/* XC_HEADER_TEMPLATE_H */
-
